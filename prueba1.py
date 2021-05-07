4547769c76bedea1a73af97533dcd34f9aa348e6
Elchidote <83800176+Elchidote@users.noreply.github.com>
2021-05-06 23:56:19 -0500

    Add files via upload

---
+++ prueba1.py
@@ -0,0 +1,16 @@
+def promedio_std(lista):
+    ## x es promedio
+    ## y es desviasion estandar
+    suma = 0
+    SumaEstandar = 0
+    i = 0
+    j = 0
+    while i < len(lista):
+       suma = suma + int(lista[i])
+       i = i + 1
+    x = suma / (len(lista))
+
+    return(x)
+
+a =[1,2,3,4,5,6,7]
+print(promedio_std([1,2,3,4,5,6,7]))
